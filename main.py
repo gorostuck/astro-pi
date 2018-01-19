8c9bc752bf30dec72e5faa80102367093162691b
Joel Pérez <joelperezferrer@gmail.com>
2018-01-19 16:58:50 +0100

    sfawrsd

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-def experimento_temperatura(t_o, t_min, t_max):
+def experimento_temperatura():
     q = 0
+    f = open('predb.txt')
+    l = f.read().split()
+    t_o = float(l[1])
+    t_min = float(l[2])
+    t_max = float(l[3])
+
     t = sense.get_temperature()
     if t > t_min and t < t_o:
         q = (t-t_min)/(t_o-t_min)
     elif t < t_max and t > t_o:
         q = (t-t_max)/(t_o-t_max)
+    f.close()
     return(q)  
+    
   
 while True:
-    print(experimento_temperatura(35, 5, 45))
+    print(experimento_temperatura())
---
+++ readfile.py
@@ -0,0 +1,4 @@
+f = open('predb.txt')
+l = f.read().split()
+print('nombre organismo: {}, temperatura optima: {} '.format(l[0], l[1]))
+    
