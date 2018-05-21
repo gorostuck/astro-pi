b8bfaf06261f95830be6900780cf440c57997162
Joel Pérez <joelperezferrer@gmail.com>
2018-05-21 10:38:46 +0200

    Actualizado con la nueva post

--- README.md
+++
@@ -1,2 +0,0 @@
-# astro-pi
-This is the repository for the group "Astropithecus" participating in Astro Pi 2017 
--- readfile.py
+++
@@ -1,4 +0,0 @@
-f = open('predb.txt')
-l = f.read().split()
-print('nombre organismo: {}, temperatura optima: {} '.format(l[0], l[1]))
-    
